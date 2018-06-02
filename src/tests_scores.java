4ac29d346827b79454d263cfcae2991015ab7273
league <league@wts10.attlocal.net>
2018-06-02 12:55:55 -0700

    today, i finished test score which is a responce and if/integer program

---
+++ src/tests_scores.java
@@ -0,0 +1,43 @@
+
+import javax.swing.JOptionPane;
+ public class tests_scores {
+	 
+	 
+	 public static void main (String[]args) {
+		 
+		 String score= JOptionPane.showInputDialog("whAT dID yoU gET on tHIS COOl LOoKING CAASP TEST WhIcH WAs SeeMInGLY HarD");
+		 int scoreNum=Integer.parseInt(score); 
+		 if(scoreNum <= 90) { 
+			 JOptionPane.showMessageDialog(null, "nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooob");
+			 		 
+			 }
+		 
+		 
+	 if(scoreNum > 90)  {
+			JOptionPane.showMessageDialog(null, "you are teh allmighty admin king thAT DESERVES ALL OF TEH ROBocks IN dIs woRLd-yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");		 
+		 
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+ }
+ }
+
+ 
